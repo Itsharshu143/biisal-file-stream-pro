51c3af30e70bb19dba132bd52de1cf34bdcaf616
Itsharshu143 <116162890+Itsharshu143@users.noreply.github.com>
2024-12-29 06:50:50 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -18,7 +18,7 @@ class Var(object):
     name = str(getenv('name', 'Harshu_file2links_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002462246320')
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002462246320'))
     NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002321282341'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
